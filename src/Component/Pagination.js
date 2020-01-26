19bc5a4031a5ccd4932b62deacf69271b4a9c2eb
hossamabdulmagid <hosamabdulmaged@gmail.com>
2020-01-27 00:20:53 +0200

    Added 2 Button

--- src/Component/Pagination.js
+++ src/Component/Pagination.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
-export function Pagination({ gotoNextPage, gotoPrevPage }) {
+ function Pagination({ gotoNextPage, gotoPrevPage }) {
     return (
         <div>
-            <Button  className="btn btn-danger"onClick={gotoPrevPage}>Previous</Button>
-            <Button className="btn btn-warning"  onClick={gotoNextPage}>Back</Button>
+            <Button className="btn btn-danger" onClick={gotoPrevPage}> Previous </Button>
+            <Button className="btn btn-warning" onClick={gotoNextPage}> Next </Button>
 
         </div>
     )
