26d03def9fd1279a65cef4b2e37af876422bcbc2
hossamabdulmagid <hosamabdulmaged@gmail.com>
2020-01-27 23:10:31 +0200

    FETCH_BUG

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react'
-import Axios from 'axios'
-import FetchPokemon from './Component/FetchPokemon'
+import React, { useState, useEffect } from 'react'
+import Fetch from "./Component/FetchPokemon"
 import './App.css'
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Users from './Pages/Users'
 function App() {
-  
+
   return (
     <>
+    <Router>
     <div className="App">
       <header className="App-header">
-        <FetchPokemon />
+        <ul>
+
+
+
+          <li className="saw">       <Link to="/">Home</Link>  </li>
+          <li className="saw">       <Link to="/About">About</Link>  </li>
+          <li className="saw">        <Link to="/Users">Users</Link> </li>
+
+        </ul>
+
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/About"  component={About} />
+      <Route path="/Users"  component={Users} />
+
+      </Switch>
+      <Fetch />
+
       </header>
     </div>
+    </Router>
     </>
   )
 }
--- src/Component/PokemonList.js
+++ src/Component/PokemonList.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import Fetch from './FetchPokemon'
+import '../App.css'
 function PokemonList({ pokemon }) {
 
     return (
-        <div>
-            {pokemon.map(p=> (
-                <div key={p}> {p} </div>
+        <Router>
+            {pokemon.map(p => (
+                <Switch>  
+
+                    <ul>
+                        
+                        <li>
+                        <Link to='/' key={p}> <Route path="">{p} </Route>  </Link>
+
+                        </li>
+                        </ul>      
+                        <Route path="/" component={Fetch} />
+                          
+                </Switch>   
+
             ))}
-        </div>
+        </Router>
     )
 }
 
